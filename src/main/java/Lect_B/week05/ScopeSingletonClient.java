eee9079b1abb0bb199161cfa1a88146249443725
정환2 <jhkim733147@naver.com>
2026-04-06 22:12:00 +0900

    Disambiguate week05 work unit injection

--- src/main/java/Lect_B/week05/ScopeSingletonClient.java
+++ src/main/java/Lect_B/week05/ScopeSingletonClient.java
@@ -1,5 +1,6 @@
 package Lect_B.week05;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,7 +8,7 @@ public class ScopeSingletonClient {
 
 	private final WorkUnit workUnit;
 
-	public ScopeSingletonClient(WorkUnit workUnit) {
+	public ScopeSingletonClient(@Qualifier("workUnit") WorkUnit workUnit) {
 		this.workUnit = workUnit;
 	}
 
